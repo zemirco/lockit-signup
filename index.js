cb50498829613eab077fd465722a21926d9346f8
zeMirco <mirco.zeiss@gmail.com>
2013-12-10 22:44:58 +0100

    added debug and improved order of routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 var path = require('path');
 var uuid = require('node-uuid');
+var ms = require('ms');
+var moment = require('moment');
+var debug = require('debug')('lockit-signup');
 
 module.exports = function(app, config) {
   
@@ -12,6 +15,7 @@ module.exports = function(app, config) {
 
   // GET /signup
   app.get(route, function(req, res) {
+    debug('GET %s', route);
     res.render(path.join(__dirname, 'views', 'get-signup'), {
       title: 'Sign up'
     });
@@ -19,6 +23,7 @@ module.exports = function(app, config) {
 
   // POST /signup
   app.post(route, function(req, response) {
+    debug('POST %s: %j', route, req.body);
 
     var username = req.body.username;
     var email = req.body.email;
@@ -41,6 +46,7 @@ module.exports = function(app, config) {
     }
 
     if (error) {
+      debug('POST error: %s', error);
       // render template with error message
       response.status(403);
       response.render(path.join(__dirname, 'views', 'get-signup'), {
@@ -55,6 +61,7 @@ module.exports = function(app, config) {
       if (err) console.log(err);
       
       if (user) {
+        debug('username already taken');
         // render template with error message
         response.status(403);
         response.render(path.join(__dirname, 'views', 'get-signup'), {
@@ -69,13 +76,17 @@ module.exports = function(app, config) {
         if (err) console.log(err);
         
         if (user) {
+          debug('email already in db');
+          
           // send already registered email
-          sendmail.emailTaken(user.username, user.email, function(err, res) {
+          var Email = new sendmail('emailSignupTaken');
+          Email.send(user.username, user.email, function(err, res) {
             if (err) console.log(err);
             response.render(path.join(__dirname, 'views', 'post-signup'), {
               title: 'Sign up - Email sent'
             });
           });
+
           return;
         }
         
@@ -86,10 +97,9 @@ module.exports = function(app, config) {
           if (err) console.log(err);
 
           // send email with link for address verification
-          sendmail.emailVerification(username, email, user.signupToken, function(err, res) {
+          var Email = new sendmail('emailSignup');
+          Email.send(user.username, user.email, user.signupToken, function(err, res) {
             if (err) console.log(err);
-
-            // render success message
             response.render(path.join(__dirname, 'views', 'post-signup'), {
               title: 'Sign up - Email sent'
             });
@@ -103,26 +113,108 @@ module.exports = function(app, config) {
     
   });
   
+  // GET /signup/resend-verification
+  app.get(route + '/resend-verification', function(req, res) {
+    debug('GET %s/resend-verification', route);
+    res.render(path.join(__dirname, 'views', 'resend-verification'), {
+      title: 'Resend verification email'
+    });
+  });
+  
+  // POST /signup/resend-verification
+  app.post(route + '/resend-verification', function(req, response) {
+    debug('POST %s/resend-verification: %j', route, req.body);
+    var email = req.body.email;
+
+    var error = null;
+    // regexp from https://github.com/angular/angular.js/blob/master/src/ng/directive/input.js#L4
+    var EMAIL_REGEXP = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/;
+
+    if (!email || !email.match(EMAIL_REGEXP)) {
+      error = 'Email is invalid';
+    }
+
+    if (error) {
+      debug('POST error: %s', error);
+      // render template with error message
+      response.status(403);
+      response.render(path.join(__dirname, 'views', 'resend-verification'), {
+        title: 'Resend verification email',
+        error: error
+      });
+      return;
+    }
+    
+    // check for user with given email address
+    adapter.find('email', email, function(err, user) {
+      if (err) console.log(err);
+
+      // no user with that email address exists -> just render success message
+      // or email address is already verified -> user has to use password reset function
+      if (!user || user.emailVerified) {
+        debug('no user found or email is not verified');
+
+        response.status(200);
+        response.render(path.join(__dirname, 'views', 'post-signup'), {
+          title: 'Sign up - Email sent'
+        });
+        return;
+      }
+      
+      // we have an existing user with provided email address
+      
+      // create new signup token
+      var token = uuid.v4();
+
+      // save token on user object
+      user.signupToken = token;
+
+      // set new sign up token expiration date
+      var timespan = ms(config.signupTokenExpiration);
+      user.signupTokenExpires = moment().add(timespan, 'ms').toDate();
+      
+      // save updated user to db
+      adapter.update(user, function(err, res) {
+        if (err) console.log(err);
+
+        // send sign up email
+        var Email = new sendmail('emailResendVerification');
+        Email.send(user.username, email, token, function(err, res) {
+          if (err) console.log(err);
+          response.render(path.join(__dirname, 'views', 'post-signup'), {
+            title: 'Sign up - Email sent'
+          });
+        });
+
+      });
+
+    });
+    
+  });
+
+  // route is at the end so it does not catch :token === 'resend-verification'
   // GET /signup/:token
   app.get(route + '/:token', function(req, response, next) {
     var token = req.params.token;
-    
+    debug('GET %s with token: %s', route, token);
+
     // verify format of token
     var re = new RegExp('[0-9a-f]{22}|[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', 'i');
 
     // if format is wrong no need to query the database
     if (!re.test(token)) return next();
-    
+
     // find user by token
     adapter.find('signupToken', token, function(err, user) {
       if (err) console.log(err);
 
       // no user found -> forward to error handling middleware
       if (!user) return next();
-      
+
       // check if token has expired
       if (new Date(user.signupTokenExpires) < new Date()) {
-        
+        debug('signup token has expired');
+
         // delete old token
         delete user.signupToken;
 
@@ -134,12 +226,12 @@ module.exports = function(app, config) {
           response.render(path.join(__dirname, 'views', 'link-expired'), {
             title: 'Sign up - Email verification link expired'
           });
-          
+
         });
-        
+
         return;
       }
-      
+
       // everything seems to be fine
 
       // set user verification values
@@ -158,89 +250,11 @@ module.exports = function(app, config) {
         response.render(path.join(__dirname, 'views', 'mail-verification-success'), {
           title: 'Sign up success'
         });
-        
-      });
-
-    });
-    
-  });
-  
-  // GET /signup/resend-verification
-  app.get(route + '/resend-verification', function(req, res) {
-    res.render(path.join(__dirname, 'views', 'resend-verification'), {
-      title: 'Resend verification email'
-    });
-  });
-  
-  // POST /signup/resend-verification
-  app.post(route + '/resend-verification', function(req, response) {
-    var email = req.body.email;
-
-    var error = null;
-    // regexp from https://github.com/angular/angular.js/blob/master/src/ng/directive/input.js#L4
-    var EMAIL_REGEXP = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/;
-
-    if (!email || !email.match(EMAIL_REGEXP)) {
-      error = 'Email is invalid';
-    }
-
-    if (error) {
-      // render template with error message
-      response.status(403);
-      response.render(path.join(__dirname, 'views', 'resend-verification'), {
-        title: 'Resend verification email',
-        error: error
-      });
-      return;
-    }
-    
-    // check for user with given email address
-    adapter.find('email', email, function(err, user) {
-      if (err) console.log(err);
-
-      // no user with that email address exists -> just render success message
-      // or email address is already verified -> user has to use password reset function
-      if (!user || user.emailVerified) {
-
-        response.status(200);
-        response.render(path.join(__dirname, 'views', 'post-signup'), {
-          title: 'Sign up - Email sent'
-        });
-        return;
-      }
-      
-      // we have an existing user with provided email address
-      
-      // create new signup token
-      var token = uuid.v4();
-
-      // save token on user object
-      user.signupToken = token;
-
-      // set new sign up token expiration date
-      var now = new Date();
-      var tomorrow = now.setTime(now.getTime() + config.signupTokenExpiration);
-      user.signupTokenExpires = new Date(tomorrow);
-      
-      // save updated user to db
-      adapter.update(user, function(err, res) {
-        if (err) console.log(err);
-
-        // send sign up email
-        sendmail.resendVerification(user.username, email, token, function(err, res) {
-          if (err) console.log(err);
-
-          // render success message
-          response.render(path.join(__dirname, 'views', 'post-signup'), {
-            title: 'Sign up - Email sent'
-          });
-
-        });
 
       });
 
     });
-    
+
   });
   
 };
